f6292b2db0093ccd3fffb0edad0cd87b15dc793a
JaeMin <dnfktmzm@naver.com>
2024-10-15 15:49:20 +0900

    git test

---
+++ src/main/java/com/api/auth/entity/Test.java
@@ -0,0 +1,44 @@
+package com.api.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "test")
+public class Test {
+    @Id
+    private int id;
+    private String name;
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+
+    // Getter와 Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+}
